347c4cf879c0f6353a2a88296bae0ffa1b437d64
abel-ronoh <abellronoh@gmail.com>
2023-11-10 10:56:30 +0300

    added a song container UI

---
+++ app/playlist/page.tsx
@@ -0,0 +1,22 @@
+export default function Playlist(){
+
+    return (
+        <>
+        <div className="absolute bg-black w-1/5 h-full ">
+
+        </div>
+        <div className="absolute bg-blue-500 w-4/5 h-full right-0">
+            <div className="flex w-full p-[1rem] bg-white justify-between">
+                <img src="" className="w-[3rem] h-[3rem]" alt="img" />
+                <div className="">
+                    <h3>Song Name</h3>
+                    <h5>Artist</h5>
+                </div>
+                <p>Album</p>
+                <p>Date added</p>
+                <p>song duration</p>
+            </div>
+        </div>
+        </>
+    )
+}
